f38c1e122e75bb125bba602387db77e52cce9642
tomefydirk <tomefydirk@yahoo.com>
2025-04-03 15:26:51 +0300

    3 avril 15:26

--- input/data/Data.java
+++ input/data/Data.java
@@ -6,23 +6,20 @@ import java.util.Vector;
 import geo.*;
 public class Data {
       public  static Zoo get_zoo(){
-        // Création des secteurs
-        Secteur s1=new SecteurPark("moyene",11,50,50,10,10);
+      
+    
         Secteur[] ls = new Secteur[6];
-        ls[3] =s1;
+    
         
-        ls[0]= new SecteurPark("Savane",100);
-        ls[0].setRect(1,1,10,10);
-
-        ls[1]= new SecteurPark("Jungle",99);
-        ls[1].setRect(10,10,20,20);
-
-        ls[2]= new SecteurPark("Zone morte",0);
-        ls[2].setRect(30,20,30,30);
-
-        ls[4] = new SecteurPark("talatamaty",64);
-        ls[4].setRect(70,20,20,20);
-        // Création des animaux
+        new SecteurPark("Savane",100,1,1,10,10).insert_into(ls);
+       
+        new SecteurPark("Jungle",99,10,10,20,20).insert_into(ls);
+     
+        new SecteurPark("Zone morte",0,30,20,30,30).insert_into(ls);
+        
+        new SecteurPark("talatamaty",64,70,20,20,20).insert_into(ls);;
+       
+        
         Vector<Anim> la = new Vector<>();
         Herbi h1 = new Herbi("h1", 90, 150, new Point(10, 10), 20);
         h1.set_dp(new Point(1,0));
@@ -30,8 +27,7 @@ public class Data {
         Herbi h2 = new Herbi("h2",9,9,new Point(9,9),9);
         h2.set_dp(new Point(-1,-1));
 
-      //  Herbi h3 = new Herbi("h3",29,98,new Point(19,9),9);
-       // h3.set_dp(new Point(-1,1));
+      
 
         Herbi h4 = new Herbi("h4",14,21,new Point(19,9),9);
         h4.set_dp(new Point(1,0));
@@ -55,22 +51,13 @@ public class Data {
         la.add(c1);
         la.add(c2);
         la.add(h2);
-        //la.add(h3);
-        //la.add(h4);
-       // la.add(h5);
-       // la.add(c3);
-       // la.add(c4);
-        // Création du zoo
-     
+        
         Zoo zoo = new Zoo(ls, la, 0, 0, 100,50);
-        if(zoo.contains(c2.get_position().getX(),c2.get_position().getY())){
-          System.out.println("vrai");
-      }
-        // Affichage de l'état initial
+      
         System.out.println("État initial du zoo :");
         zoo.afficher_la();
         
-        // Déplacement et interactions des animaux
+      
        
         
         
